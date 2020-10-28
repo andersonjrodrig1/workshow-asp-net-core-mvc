38de942a5196c4744484067192f0729d7d9398cc
Anderson Junior Rodrigues <andersonkz@ymail.com>
2020-10-28 00:33:39 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,21 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Sellers = new List<Seller>();
+        }
+
+        public void AddSeller(Seller seller) => Sellers.Add(seller);
+
+        public double TotalSales(DateTime initial, DateTime final) => Sellers.Sum(x => x.TotalSales(initial, final));
     }
 }
