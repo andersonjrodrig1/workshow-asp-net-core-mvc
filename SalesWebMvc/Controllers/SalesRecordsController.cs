7a8309abfd8c4331703225d9733211aa91c6aae9
Anderson Junior Rodrigues <andersonkz@ymail.com>
2020-12-07 23:41:21 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public SalesRecordsController()
+        {
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        //GET: SalesRecords/SimpleSearch
+        public async Task<IActionResult> SimpleSearch()
+        {
+            return View();
+        }
+
+        //GET: SalesRecords/GroupingSearch
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
